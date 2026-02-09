55e53ec445e8979457770cde21f56c55cec8eaad
mrpux <mrpuxdev@gmail.com>
2026-02-09 15:35:19 -0500

    rebuilt

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/NextHorizon/",       
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react()].filter(Boolean),
+export default defineConfig({
+  base: "/NextHorizon/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
+
