dafd4c618205849fada78dfc41264e41e1b4afcb
mrpux <mrpuxdev@gmail.com>
2026-02-09 15:29:04 -0500

    rebuilt

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/NextHorizon/",       
   server: {
     host: "::",
     port: 8080,
